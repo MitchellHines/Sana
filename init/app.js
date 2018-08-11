77eaa0395f9c442e40da3223a89e4162fc52c933
Shiraz Chokshi <shirazchokshi@Shirazs-MacBook-Pro.local>
2018-08-11 18:16:01 -0400

    added navbar and routes functionality

--- init/app.js
+++ init/app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var playground = require('./routes/playground.js');
+var map = require('./routes/map.js');
+var donate = require('./routes/donate.js');
+var media = require('./routes/media.js');
 
 var app = express();
 
@@ -21,6 +25,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/playground', playground);
+app.use('/map', map);
+app.use('/donate', donate);
+app.use('/media', media);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ init/routes/media.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* Renders Recipe view */
+router.get('/:id', function(req, res) {
+  
+});
+
+module.exports = router;
