ce2a852aab5a1372a699f3604507a46adb66f3bd
MitchellHines <mitchell.hines@capgemini-gs.com>
2018-08-11 21:05:46 -0400

    working on 4 pages

--- init/routes/donate.js
+++ init/routes/donate.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* Renders Recipe view */
-router.get('/donate', function(req, res) {
-  res.render('donate', { title: 'Donate' });
+router.get('/', function(req, res) {
+  res.render('donate', { title: 'Express' });
 });
 
 module.exports = router;
--- init/routes/map.js
+++ init/routes/map.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* Renders Recipe view */
-router.get('/map', function(req, res) {
-  res.render('map', { title: 'Map' });
+router.get('/', function(req, res) {
+  res.render('map', { title: 'Express' });
 });
 
 module.exports = router;
